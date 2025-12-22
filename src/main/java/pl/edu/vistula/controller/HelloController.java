e1e9f14236453b81e28ced7959c58c2c1dc47a2b
AldiyarZhalgas <azhalga1@stu.vistula.edu.pl>
2025-12-22 03:10:00 +0100

    Commit first

--- src/main/java/pl/edu/vistula/controller/HelloController.java
+++ src/main/java/pl/edu/vistula/controller/HelloController.java
@@ -3,13 +3,20 @@ package pl.edu.vistula.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
+    @GetMapping("/")
+    @ResponseBody
+    public String hello() {
+        return "Hello Vistula, my first Spring controller!";
+    }
+
     @GetMapping("/greeting")
     public String greeting(Model model) {
-        model.addAttribute("name", "Aldiyar");
+        model.addAttribute("message", "Hello Vistula, my first Spring controller!");
         return "greeting";
     }
 }
