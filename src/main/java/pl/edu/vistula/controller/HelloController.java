81417d31b6a62aaf393111667931421980f29fdd
AldiyarZhalgas <azhalga1@stu.vistula.edu.pl>
2025-12-22 02:58:28 +0100

    Commit first

---
+++ src/main/java/pl/edu/vistula/controller/HelloController.java
@@ -0,0 +1,15 @@
+package pl.edu.vistula.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/greeting")
+    public String greeting(Model model) {
+        model.addAttribute("name", "Aldiyar");
+        return "greeting";
+    }
+}
